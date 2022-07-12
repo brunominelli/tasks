20f0185c94db9632c4be11ecae1d564259fed59c
Bruno Minelli <obrunominelli@gmail.com>
2022-07-12 12:59:37 -0300

    feat: Identação da camada de controladores

--- backend/src/controllers/tasksController.js
+++ backend/src/controllers/tasksController.js
@@ -6,10 +6,12 @@ const Tasks = {
     const tasks = await TasksService.createTask(task, deadline);
     response.status(201).json(tasks);
   },
+
   async readTasks(_request, response) {
     const tasks = await TasksService.readTasks();
     response.status(200).json(tasks);
   },
+
   async readTaskById(request, response) {
     const { id } = request.params;
 
@@ -17,12 +19,14 @@ const Tasks = {
 
     response.status(200).json(tasks);
   },
+
   async updateTask(request, response) {
     const { id } = request.params;
     const { task, deadline } = request.body;
     const tasks = await TasksService.updateTask(Number(id), task, deadline);
     response.status(200).json(tasks);
   },
+
   async deleteTask(request, response) {
     const { id } = request.params;
     await TasksService.deleteTask(Number(id));
